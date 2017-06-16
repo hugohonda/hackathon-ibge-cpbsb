3f19354017ec605082a6fde09b325f3dd9c22517
lucas brilhante <lucas brilhante>
2017-06-16 16:10:49 -0300

    Criando projeto

---
+++ AbreAqui/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AbreAqui.Startup))]
+namespace AbreAqui
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
