fa76732abe556d7fafe2539d1a65098523944136
hugohonda <hugohondaf@gmail.com>
2017-06-17 03:45:27 -0300

    upload vue frontend

---
+++ frontend-ibge/src/components/Home.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="home">
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="home-header mdl-layout__header">
+        <div class="mdl-layout__header-row">
+          <!-- Title -->
+          <span class="home-header-title mdl-layout-title">{{ title }}</span>
+          <!-- Add spacer, to align navigation to the right -->
+          <div class="mdl-layout-spacer"></div>
+          <!-- Navigation. We hide it in small screens. -->
+          <nav class="mdl-navigation mdl-layout--large-screen-only">
+            <a class="mdl-navigation__link" href="">Cadastro</a>
+            <a class="mdl-navigation__link" href="">Login</a>
+          </nav>
+        </div>
+      </header>
+      <div class="home-drawer mdl-layout__drawer">
+        <span class="mdl-layout-title">{{ title }}</span>
+        <nav class="mdl-navigation">
+          <a class="mdl-navigation__link" href="">Cadastro</a>
+          <a class="mdl-navigation__link" href="">Login</a>
+        </nav>
+      </div>
+      <main class="mdl-layout__content">
+        <div class="home-main-section mdl-typography--text-center">
+          <div class="logo-font home-slogan">Abre Aqui</div>
+          <div class="logo-font home-sub-slogan">Onde seu negócio dá certo!</div>
+          <div class="input-box-container">
+            <input-location :text="'Localização'" :icon="'location_on'"></input-location>
+            <input-box :text="'Bairro'" :icon="'person_pin_circle'"></input-box>
+            <input-box :text="'Tipo de negócio'" :icon="'store'"></input-box>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import InputBox from '@/components/InputBox'
+import InputLocation from '@/components/InputLocation'
+import PlaceInput from 'vue-google-maps'
+
+export default {
+  name: 'home',
+  components: {
+    InputBox
+  },
+  data () {
+    return {
+      title: 'Abre Aqui',
+      vm: {
+        searchPlace: '',
+        location: {}
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.home-header {
+  overflow: visible;
+}
+
+.home-header .material-icons {
+  color: #fff !important;
+}
+
+.home-header .mdl-layout__drawer-button {
+  background: transparent;
+  color: #fff;
+}
+
+.home-header .mdl-navigation__link {
+  color: #fff;
+  font-weight: 700;
+  font-size: 14px;
+}
+
+  .home-header-title {
+    color: #fff;
+    font-weight: 700;
+    font-size: 24px;
+  }
+
+.logo-font {
+  font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+  line-height: 1;
+  color: #FFF;
+  font-weight: 500;
+}
+
+.home-slogan {
+  color: #ffffff;
+  font-size: 80px;
+  padding-top: 80px;
+}
+
+.home-sub-slogan {
+  color: #ffffff;
+  font-size: 41px;
+  padding-top: 24px;
+}
+
+.home-main-section {
+  position: relative;
+  height: 800px;
+  width: auto;
+  background-color: #f3f3f3;
+  background: url('../assets/ipanema-up.png') center 30% no-repeat;
+  background-size: cover;
+}
+
+.input-box-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-content: center;
+  align-items: center;
+  justify-content: space-around;
+  padding: 2em;
+  margin-top: 2em;
+}
+
+
+</style>
---
+++ frontend-ibge/src/components/InputBox.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="input-box">
+    <div class="input-container mdl-card mdl-shadow--4dp">
+      <i class="input-icon material-icons">{{ icon }}</i>
+      <form action="#">
+        <div class="input-text mdl-textfield mdl-js-textfield">
+          <input class="mdl-textfield__input" type="text" id="location-input">
+          <label class="mdl-textfield__label" for="location-input">{{ text }}</label>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'input-box',
+  props: {
+    text: {
+      type: String
+    },
+    icon: {
+      type: String
+    }
+  },
+  data () {
+    return {
+      msg: 'InputBox'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+    .input-box {
+      padding: 2em;
+    }
+
+    .input-container.mdl-card {
+      display: flex;
+      flex-direction: row;
+      min-width: 360px;
+      min-height: 64px;
+      height: 64px;
+      align-items: center;
+      justify-content: space-around;
+    }
+
+    .input-icon {
+      padding-right: 1em;
+      padding-left: 1em;
+    }
+
+    .input-text {
+      padding-right: 2em;
+    }
+
+</style>
